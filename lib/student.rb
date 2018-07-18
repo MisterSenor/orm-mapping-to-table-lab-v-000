3abdff9634fbf1fc3e9feb86457a6a8db439d691
Nathan  Visak <nathan.visak@gmail.com>
2018-07-18 21:10:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,4 +23,13 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def save 
+    #remember that this is an instance method
+    sql = <<-SQL
+    INSERT INTO students (name, grade) VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end 
+
 end
